6f689823bce72a8501395718603df882e915a075
Aluno <Aluno@SN-279232>
2019-02-05 19:51:42 -0200

    *ADCIONAMOS UMA INTERFACE *CLASSE DE CÁLCULO DO IMC *aDCIONADAS LABELS PARA TRATAMENTO DOS TEXTOS

---
+++ src/calculoimc/CalculoImc.java
@@ -0,0 +1,20 @@
+
+package calculoimc;
+
+import javax.swing.JFrame;
+import telas.PainelPrincipal;
+
+public class CalculoImc {
+
+    public static void main(String[] args) {
+        JFrame janela = new JFrame("Calculadora de IMC");
+        PainelPrincipal painel = new PainelPrincipal();
+        
+        //Define atributos da TELA
+        janela.setDefaultCloseOperation(janela.EXIT_ON_CLOSE);
+        janela.setSize(430, 398);
+        janela.add(painel);
+        janela.setVisible(true);
+    }
+    
+}
---
+++ src/calculoimc/Imc.java
@@ -0,0 +1,25 @@
+
+package calculoimc;
+
+public class Imc {
+    public double resultado;
+    public double calcular(double peso, double altura){
+        resultado = peso/(altura*altura);
+        return resultado;
+    }
+    public String situacao(String situation){
+        if(this.resultado >= 18.5 && this.resultado <= 24.99){
+            situation = "normal";
+        }else if(this.resultado >= 25 && this.resultado <= 29.99){
+            situation = "GORDO(A)";
+        }else if(this.resultado >= 30 && this.resultado <= 34.99){
+            situation = "GORDO(A) PRA CARALHO";
+        }else if(this.resultado >= 35 && this.resultado <= 39.99){
+            situation = "MONTANHA DE MERDA / ISMAR";
+        }else if(this.resultado > 40){
+            situation = "O LIXO MAIS DESUMANO DO MUNDO";
+        }      
+            return situation;
+    }
+}
+
